b98359cf924e4c1a97d1a86995fadd11c49c7bfe
Fabian <rostellofabian@gmail.com>
2026-03-03 09:29:36 +0100

    feat: add mock for server

---
+++ mock/mock.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import {rss_feed} from './rss_text.js'
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/mock', async (req, res) => {
+    try {
+        res.status(200).json(rss_feed);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+
+console.log("Listening on http://localhost:3002");
+app.listen(3002);
