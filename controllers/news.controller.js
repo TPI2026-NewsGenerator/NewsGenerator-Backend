3ea2ff3e711b66944e8527694b18f7807b0ef257
Fabian <rostellofabian@gmail.com>
2026-03-23 16:20:48 +0100

    feat: add data/filters validation

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -3,7 +3,43 @@
 import NewsService from '../services/news.service.js';
 
 const News = {
-    async fetch(req, res) {
+    getNews: async (req, res) => {
+        const filters = req.body;
+
+        // authorized filters
+        const authorizedFilters = ['keywords', 'categories', 'undesiredTopics', 'language', 'timeframe'];
+        for (let filter in filters) {
+            if (!authorizedFilters.find(element => element === filter)) {
+                const err = new Error(`Filter ${filter} is not authorized.`);
+                err.status = 400;
+                throw err;
+            }
+        }
+
+        const {keywords, categories, undesiredTopics, language, timeframe} = filters;
+
+        // validate data
+        if (keywords === undefined || keywords.length === 0) {
+            const err = new Error("No keywords were inserted, please enter at least one.")
+            err.status = 400;
+            throw err;
+        }
+
+        if (categories === undefined || categories.length === 0) {
+            const err = new Error("No categories selected, please select at least one category.")
+            err.status = 400;
+            throw err;
+        }
+
+        const authorizedLang = ['en', 'fr', 'es', 'ch', 'ru'];
+        if (language !== undefined || language !== "") {
+            if (!authorizedLang.find(element => element === language)) {
+                const err = new Error(`Language ${language} is not authorized.`);
+                err.status = 400;
+                throw err;
+            }
+        }
+
         try {
             const customer = await NewsService.fetch(req.body);
             res.status(200).json(customer);
