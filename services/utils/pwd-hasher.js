bbee1b2075064e0a00815fac2d8cf9a8e20ed0c1
Fabian <rostellofabian@gmail.com>
2026-04-30 10:31:09 +0200

    feat: add salt to hashing

--- services/utils/pwd-hasher.js
+++ services/utils/pwd-hasher.js
@@ -1,12 +1,13 @@
 import bcrypt from 'bcrypt'
 
-const hashPassword = async (password) => {
-    const saltRounds = 12
+export const hashWithSalt = async (password) => {
+    const saltRounds = 12 // 2^12 iterations
     const hash = await bcrypt.hash(password, saltRounds)
+
     return hash
 }
 
-const verifyPassword = async (password, hash) => {
+export const verifyPassword = async (password, hash) => {
     const match = await bcrypt.compare(password, hash)
     return match
 }
