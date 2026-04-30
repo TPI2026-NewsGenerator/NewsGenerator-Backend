006d58468171523de152dd6a7e6eb7a134f1cb27
Fabian <rostellofabian@gmail.com>
2026-04-30 11:59:06 +0200

    chore: line separators differences (weird commit)

--- tests/mock/mock.js
+++ tests/mock/mock.js
@@ -1,20 +1,1 @@
-import express from "express";
-import cors from "cors";
-import {rssFeed} from './rss-feed.js'
-
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.get('/api/mock', async (req, res) => {
-    try {
-        res.status(200).json(rssFeed);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-
-console.log("Listening on http://localhost:3002");
-app.listen(3002);
+import express from "express";import cors from "cors";import {rssFeed} from './rss-feed.js'const app = express();app.use(express.json());app.use(cors());app.get('/api/mock', async (req, res) => {    try {        res.status(200).json(rssFeed);    } catch (err) {        res.status(500).json({error: err.message});    }});console.log("Listening on http://localhost:3002");app.listen(3002);
