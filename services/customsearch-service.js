40f90ab87eb1321b7c5b67bfcd0573db4dc6d5e0
Fabian <rostellofabian@gmail.com>
2026-05-11 10:45:05 +0200

    feat: update get for service + response

--- services/customsearch-service.js
+++ services/customsearch-service.js
@@ -3,16 +3,32 @@
 import { CustomSearchModel } from '../models/customsearch-model.js'
 
 export const CustomSearchService = {
-    getUserCustomSearch: async ({user_id}) => {
-        // get user
-        const user = await CustomSearchModel.getUserCustomSearch(user_id)
+    getUserCustomSearch: async ({id}) => {
+        if (!id) {
+            const err = new Error(`A id is required...`);
+            err.status = 400;
+            throw err;
+        }
 
-        if (!user) {
+        const response = await CustomSearchModel.getUserCustomSearch(parseInt(id))
+
+        if (!response) {
             const err = new Error(`This user_id does not exist...`);
             err.status = 404;
             throw err;
         }
 
         return {id_user: user.id};
+        return response.custom_searches.map(search => {
+            return {
+                id: search.id,
+                title: search.title,
+                keyword: search.keyword,
+                language: search.language,
+                timeframe: search.timeframe,
+                category: search.custom_searches_has_categories.map(selected => selected.categories.category_name.toLowerCase())
+            }
+        })
+    },
     }
 }
