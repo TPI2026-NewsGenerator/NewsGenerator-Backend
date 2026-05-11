14c110ab82ef21a2b933bee8618a559f0cbd4413
Fabian <rostellofabian@gmail.com>
2026-05-11 10:43:41 +0200

    refractor: variable name changed

--- controllers/customsearch-controller.js
+++ controllers/customsearch-controller.js
@@ -5,10 +5,10 @@ import {CustomSearchService} from '../services/customsearch-service.js';
 export const CustomSearchController = {
     getUserCustomSearch: async (req, res) => {
         try {
-            const news = await CustomSearchService.getUserCustomSearch(req.body);
-            res.status(200).json(news);
+            const customSearch = await CustomSearchService.getUserCustomSearch(req.query);
+            res.status(200).json(customSearch);
         } catch (error) {
-            if (typeof(error) === 'string' && error.includes("None of theses categories were found:")) {
+            if (typeof(error) === 'string' && error.includes("None of theses custom searches were found:")) {
                 res.status(400).json({error: error});
             } else {
                 res.status(500).json({error: error});
