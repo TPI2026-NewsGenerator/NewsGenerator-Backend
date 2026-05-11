de45250c71f0014a858944293a2cfc3c3721d8c2
Fabian <rostellofabian@gmail.com>
2026-05-11 10:48:50 +0200

    feat: add post for controller

--- controllers/customsearch-controller.js
+++ controllers/customsearch-controller.js
@@ -16,4 +16,16 @@ export const CustomSearchController = {
         }
     },
 
+    postUserCustomSearch: async (req, res) => {
+        try {
+            const customSearch = await CustomSearchService.postUserCustomSearch(req.body);
+            res.status(200).json(customSearch);
+        } catch (error) {
+            if (typeof(error) === 'string' && error.includes("Error saving this custom search:")) {
+                res.status(400).json({error: error});
+            } else {
+                res.status(500).json({error: error});
+            }
+        }
+    },
 }
